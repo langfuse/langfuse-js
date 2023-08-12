6fce8c05d86778fb184eca6c2cb1dcdfd2481302
Marc Klingen <git@marcklingen.com>
2023-08-12 02:15:51 +0200

    Add sdk info to request headers

---
+++ langfuse-core/test/langfuse.headers.spec.ts
@@ -0,0 +1,40 @@
+import {
+  createTestClient,
+  type LangfuseCoreTestClient,
+  type LangfuseCoreTestClientMocks,
+} from "./test-utils/LangfuseCoreTestClient";
+
+describe("Langfuse Core", () => {
+  let langfuse: LangfuseCoreTestClient;
+  let mocks: LangfuseCoreTestClientMocks;
+
+  jest.useFakeTimers();
+
+  beforeEach(() => {
+    delete process.env.LANGFUSE_RELEASE;
+    [langfuse, mocks] = createTestClient({
+      publicKey: "pk-lf-111",
+      secretKey: "sk-lf-111",
+      flushAt: 1,
+    });
+  });
+
+  describe("Headers", () => {
+    it("should create a trace", async () => {
+      langfuse.trace({
+        name: "test-trace",
+      });
+
+      expect(mocks.fetch).toHaveBeenCalledTimes(1);
+      // cehck headers
+      const options = mocks.fetch.mock.calls[0][1];
+      expect(options.method).toBe("POST");
+      expect(options.headers).toMatchObject({
+        "Content-Type": "application/json",
+        "X-Langfuse-Sdk-Name": "langfuse-js",
+        "X-Langfuse-Sdk-Version": langfuse.getLibraryVersion(),
+        "X-Langfuse-Sdk-Variant": langfuse.getLibraryId(),
+      });
+    });
+  });
+});
