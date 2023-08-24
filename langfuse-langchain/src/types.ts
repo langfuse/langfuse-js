97fce31075c74b02349a59783f49084265578a75
paulburkhardt <92929336+paulburkhardt@users.noreply.github.com>
2023-08-24 22:19:57 +0200

    Implemented Langchain Callback Wrapper in TS

---
+++ langfuse-langchain/src/types.ts
@@ -0,0 +1,7 @@
+import { type LangfuseCoreOptions } from "../../langfuse-core/src";
+
+export type LangfuseOptions = {
+  // autocapture?: boolean
+  persistence?: "localStorage" | "sessionStorage" | "cookie" | "memory";
+  persistence_name?: string;
+} & LangfuseCoreOptions;
