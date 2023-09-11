ba413a4dd2ffcf2f17cb7b47404b3b109025808e
Max Deichmann <m.deichmann@tum.de>
2023-09-11 17:44:17 +0200

    fix export

---
+++ integration-test/langfuse-integration-langfuse.spec.ts
@@ -0,0 +1,29 @@
+// uses the compiled node.js version, run yarn build after making changes to the SDKs
+import CallbackHandler from "../langfuse-langchain";
+import Langfuse from "../langfuse-langchain";
+
+const LF_HOST = process.env.LF_HOST ?? "http://localhost:3000";
+const LF_PUBLIC_KEY = process.env.LF_PUBLIC_KEY ?? "pk-lf-1234567890";
+const LF_SECRET_KEY = process.env.LF_SECRET_KEY ?? "sk-lf-1234567890";
+
+describe("Langfuse Langchain", () => {
+  describe("core", () => {
+    it("exports the Langfuse SDK", () => {
+      const langfuse = new Langfuse({
+        publicKey: LF_PUBLIC_KEY,
+        secretKey: LF_SECRET_KEY,
+        baseUrl: LF_HOST,
+      });
+      expect(langfuse).toBeInstanceOf(Langfuse);
+    });
+
+    it("exports the callback handler", async () => {
+      const callbackHandler = new CallbackHandler({
+        publicKey: LF_PUBLIC_KEY,
+        secretKey: LF_SECRET_KEY,
+        baseUrl: LF_HOST,
+      });
+      expect(callbackHandler).toBeInstanceOf(CallbackHandler);
+    });
+  });
+});
--- langfuse-langchain/index.ts
+++ langfuse-langchain/index.ts
@@ -1,3 +1,7 @@
 import { CallbackHandler } from "./src/callback";
 
 export default CallbackHandler;
+
+export * from "./src/callback";
+
+export { Langfuse } from "../langfuse";
