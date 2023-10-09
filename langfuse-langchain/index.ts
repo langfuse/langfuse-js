ad92ceb6b591f713947b2cb23bbf4a589e77d155
Max Deichmann <m.deichmann@tum.de>
2023-10-09 23:03:16 +0200

    fix

--- langfuse-langchain/index.ts
+++ langfuse-langchain/index.ts
@@ -1,7 +1,2 @@
-import { CallbackHandler } from "./src/callback";
-
-export default CallbackHandler;
-
-export * from "./src/callback";
-
+export { CallbackHandler } from "./src/callback";
 export { Langfuse } from "../langfuse";
