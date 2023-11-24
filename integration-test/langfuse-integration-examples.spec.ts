1def2785237bd7a77c4efd78afef9bd9c00156fe
Johannes Goslar <jogo@kronberger-spiele.de>
2023-11-24 18:44:13 +0100

    install dependencies for examples in integration tests

--- integration-test/langfuse-integration-examples.spec.ts
+++ integration-test/langfuse-integration-examples.spec.ts
@@ -1,9 +1,29 @@
 import { spawnSync } from "child_process";
 import { join } from "path";
 
+function prepareExample(cwd: string): void {
+  const yarnResult = spawnSync("yarn", {
+    cwd,
+    encoding: "utf-8",
+    stdio: "inherit",
+  });
+
+  expect(yarnResult.status).toBe(0);
+
+  const resultDependencies = spawnSync("npm", ["run", "build:dependencies"], {
+    cwd,
+    encoding: "utf-8",
+    stdio: "inherit",
+  });
+
+  expect(resultDependencies.status).toBe(0);
+}
+
 describe("Test Node Example", () => {
   it("runs", () => {
     const cwd = join(__dirname, "..", "examples", "example-node");
+    prepareExample(cwd);
+
     const result = spawnSync("npm", ["run", "start"], {
       cwd,
       encoding: "utf-8",
@@ -17,13 +37,7 @@ describe("Test Node Example", () => {
 describe("Test Web Example", () => {
   it("builds", () => {
     const cwd = join(__dirname, "..", "examples", "example-web");
-    const resultDependencies = spawnSync("npm", ["run", "build:dependencies"], {
-      cwd,
-      encoding: "utf-8",
-      stdio: "inherit",
-    });
-
-    expect(resultDependencies.status).toBe(0);
+    prepareExample(cwd);
 
     const result = spawnSync("npm", ["run", "build"], {
       cwd,
@@ -38,13 +52,7 @@ describe("Test Web Example", () => {
 describe("Test Web Next.js Example", () => {
   it("builds", () => {
     const cwd = join(__dirname, "..", "examples", "example-web-nextjs");
-    const resultDependencies = spawnSync("npm", ["run", "build:dependencies"], {
-      cwd,
-      encoding: "utf-8",
-      stdio: "inherit",
-    });
-
-    expect(resultDependencies.status).toBe(0);
+    prepareExample(cwd);
 
     const result = spawnSync("npm", ["run", "build"], {
       cwd,
