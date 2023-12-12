dc2512edcd1def9bceed06ba42876051698f9c95
Max Deichmann <m.deichmann@tum.de>
2023-12-13 00:14:48 +0100

    improvements

--- integration-test/langfuse-integration-modules.spec.ts
+++ integration-test/langfuse-integration-modules.spec.ts
@@ -9,7 +9,7 @@ describe("Test Different Module Conventions", () => {
         cwd: join(__dirname, "modules"),
         encoding: "utf-8",
       });
-
+      console.log(result.stdout);
       expect(result.status).toBe(0);
       expect(result.stdout).toContain("Did construct objects and called them.");
     }
