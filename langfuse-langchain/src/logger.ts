b912d457395befecab6f8cff2aa2847ce5ad6743
Max Deichmann <m.deichmann@tum.de>
2024-01-11 15:30:29 +0100

    remove logger

--- langfuse-langchain/src/logger.ts
+++
@@ -1,8 +0,0 @@
-import winston from "winston";
-
-export const createLogger = (debug: boolean): winston.Logger =>
-  winston.createLogger({
-    format: winston.format.json(),
-    transports: [new winston.transports.Console()],
-    level: debug ? "debug" : "info",
-  });
