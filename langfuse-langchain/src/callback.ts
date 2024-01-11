489e1d1f134029f7434a0ef0657402c5d023578a
Max Deichmann <m.deichmann@tum.de>
2024-01-11 15:58:01 +0100

    fix

--- langfuse-langchain/src/callback.ts
+++ langfuse-langchain/src/callback.ts
@@ -40,7 +40,6 @@ export class CallbackHandler extends BaseCallbackHandler {
   sessionId?: string;
   rootProvided: boolean = false;
   debugEnabled: boolean = false;
-  debugEnabled: boolean = false;
 
   constructor(params: ConstructorParams) {
     super();
