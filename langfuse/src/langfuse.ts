35a64cf12eef19df0a04a36520433891ea9717d1
RichardKruemmel <richard.kruemmel@code.berlin>
2024-06-20 14:21:17 +0200

    feat: Add LangfuseWeb docs

--- langfuse/src/langfuse.ts
+++ langfuse/src/langfuse.ts
@@ -8,7 +8,7 @@ import {
 } from "langfuse-core";
 import { type LangfuseStorage, getStorage } from "./storage";
 import { version } from "../package.json";
-import { type LangfuseOptions } from "./types";
+import { type LangfuseWebOptions, type LangfuseOptions } from "./types";
 
 // Required when users pass these as typed arguments
 export {
@@ -83,12 +83,103 @@ export class Langfuse extends LangfuseCore {
   }
 }
 
+/**
+ * LangfuseWeb client
+ * The langfuse JS/TS SDK can be used to report scores client-side directly from the browser. It is commonly used to ingest scores into Langfuse which are based on implicit user interactions and feedback.
+ * Hint for Next.js users: you need to prefix the public key with NEXT_PUBLIC_ to expose it in the frontend.
+ *
+ * @param {LangfuseWebOptions} params - LangfuseWebOptions object to configure the LangfuseWeb client.
+ * @returns {LangfuseWeb} - LangfuseWeb client instance.
+ *
+ * @example
+ * ```typescript
+ * import { LangfuseWeb } from "langfuse";
+ *
+ * const langfuseWeb = new LangfuseWeb({
+ *   publicKey: "pk-lf-...",
+ *   baseUrl: "https://cloud.langfuse.com", // 🇪🇺 EU region
+ *   // baseUrl: "https://us.cloud.langfuse.com", // 🇺🇸 US region
+ * });
+ *
+ * // React example
+ * const handleUserFeedback = async (value: number) => {
+ *   await langfuseWeb.score({
+ *     traceId: props.traceId,
+ *     name: "user_feedback",
+ *     value,
+ *   });
+ * };
+ *
+ * return (
+ *   <div>
+ *     <button onClick={() => handleUserFeedback(1)}>👍</button>
+ *     <button onClick={() => handleUserFeedback(0)}>👎</button>
+ *   </div>
+ * );
+ * ```
+ *
+ * ```vue
+ * // Vue example
+ * <template>
+ *   <div>
+ *     <button @click="handleUserFeedback(1)">👍</button>
+ *     <button @click="handleUserFeedback(0)">👎</button>
+ *   </div>
+ * </template>
+ *
+ * <script>
+ *   import { LangfuseWeb } from "langfuse";
+ *
+ *   export default {
+ *     props: {
+ *       traceId: {
+ *         type: String,
+ *         required: true,
+ *       },
+ *     },
+ *     data() {
+ *       return {
+ *         langfuseWeb: null,
+ *       };
+ *     },
+ *     created() {
+ *       this.langfuseWeb = new LangfuseWeb({
+ *         publicKey: process.env.VUE_APP_LANGFUSE_PUBLIC_KEY,
+ *         baseUrl: "https://cloud.langfuse.com", // 🇪🇺 EU region
+ *         // baseUrl: "https://us.cloud.langfuse.com", // 🇺🇸 US region
+ *       });
+ *     },
+ *     methods: {
+ *       async handleUserFeedback(value) {
+ *         await this.langfuseWeb.score({
+ *           traceId: this.traceId,
+ *           name: "user_feedback",
+ *           value,
+ *         });
+ *       },
+ *     },
+ *   };
+ * </script>
+ * ```
+ *
+ * ```typescript
+ * // Score example
+ * // pass traceId and observationId to front end
+ * await langfuseWeb.score({
+ *   traceId: message.traceId,
+ *   observationId: message.observationId,
+ *   name: "user-feedback",
+ *   value: 1,
+ *   comment: "I like how personalized the response is",
+ * });
+ * ```
+ */
 export class LangfuseWeb extends LangfuseWebStateless {
   private _storage: LangfuseStorage;
   private _storageCache: any;
   private _storageKey: string;
 
-  constructor(params?: Omit<LangfuseOptions, "secretKey">) {
+  constructor(params?: LangfuseWebOptions) {
     const langfuseConfig = utils.configLangfuseSDK(params, false);
     super(langfuseConfig);
 
