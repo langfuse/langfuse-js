7e26c29e6c13b33ada7b8753502055fb71ab1104
RichardKruemmel <richard.kruemmel@code.berlin>
2024-07-26 11:41:55 +0200

    chore: Refactor GetLangfuseObservationsQuery interface and update types.ts

--- langfuse-core/src/types.ts
+++ langfuse-core/src/types.ts
@@ -189,33 +189,33 @@ export type GetLangfuseObservationsQuery = FixTypes<paths["/api/public/observati
 /**
  * GetLangfuseObservationsResponse
  * @property data - List of observations.
- * @property data[].modelId - The model ID associated with the observation.
- * @property data[].inputPrice - The input price of the observation.
- * @property data[].outputPrice - The output price of the observation.
- * @property data[].totalPrice - The total price of the observation.
- * @property data[].calculatedInputCost - The calculated input cost of the observation.
- * @property data[].calculatedOutputCost - The calculated output cost of the observation.
- * @property data[].calculatedTotalCost - The calculated total cost of the observation.
- * @property data[].latency - The latency of the observation.
- * @property data[].timeToFirstToken - The time to first token of the observation.
- * @property data[].id - The ID of the observation.
- * @property data[].traceId - The trace ID associated with the observation.
- * @property data[].type - The type of the observation.
- * @property data[].name - The name of the observation.
- * @property data[].startTime - The start time of the observation.
- * @property data[].endTime - The end time of the observation.
- * @property data[].completionStartTime - The completion start time of the observation.
- * @property data[].model - The model used for the observation.
- * @property data[].modelParameters - The parameters of the model used for the observation.
- * @property data[].input - The input data of the observation.
- * @property data[].version - The version of the observation.
- * @property data[].metadata - Additional metadata of the observation.
- * @property data[].output - The output data of the observation.
- * @property data[].usage - The usage data of the observation.
- * @property data[].level - The level of the observation.
- * @property data[].statusMessage - The status message of the observation.
- * @property data[].parentObservationId - The parent observation ID.
- * @property data[].promptId - The prompt ID associated with the observation.
+ * @property data.modelId - The model ID associated with the observation.
+ * @property data.inputPrice - The input price of the observation.
+ * @property data.outputPrice - The output price of the observation.
+ * @property data.totalPrice - The total price of the observation.
+ * @property data.calculatedInputCost - The calculated input cost of the observation.
+ * @property data.calculatedOutputCost - The calculated output cost of the observation.
+ * @property data.calculatedTotalCost - The calculated total cost of the observation.
+ * @property data.latency - The latency of the observation.
+ * @property data.timeToFirstToken - The time to first token of the observation.
+ * @property data.id - The ID of the observation.
+ * @property data.traceId - The trace ID associated with the observation.
+ * @property data.type - The type of the observation.
+ * @property data.name - The name of the observation.
+ * @property data.startTime - The start time of the observation.
+ * @property data.endTime - The end time of the observation.
+ * @property data.completionStartTime - The completion start time of the observation.
+ * @property data.model - The model used for the observation.
+ * @property data.modelParameters - The parameters of the model used for the observation.
+ * @property data.input - The input data of the observation.
+ * @property data.version - The version of the observation.
+ * @property data.metadata - Additional metadata of the observation.
+ * @property data.output - The output data of the observation.
+ * @property data.usage - The usage data of the observation.
+ * @property data.level - The level of the observation.
+ * @property data.statusMessage - The status message of the observation.
+ * @property data.parentObservationId - The parent observation ID.
+ * @property data.promptId - The prompt ID associated with the observation.
  * @property meta - Metadata about the response.
  * @property meta.page - The current page number.
  * @property meta.limit - The number of items per page.
