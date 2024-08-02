b9b3f2de43a6aa410e9b26195b2616ce8e03c895
Hassieb Pakzad <68423100+hassiebp@users.noreply.github.com>
2024-08-02 11:37:23 +0200

    add more tests

--- integration-test/langfuse-integration-vercel.spec.ts
+++ integration-test/langfuse-integration-vercel.spec.ts
@@ -7,6 +7,7 @@ import { NodeSDK } from "@opentelemetry/sdk-node";
 import z from "zod";
 import { LangfuseVercelSpanExporter } from "../langfuse-vercel";
 import { randomUUID } from "crypto";
+import { fetchTraceById } from "./integration-utils";
 
 jest.useRealTimers();
 
@@ -34,59 +35,233 @@ describe("langfuse-integration-vercel", () => {
     sdk.start();
   });
 
-  afterEach(async () => {
-    await sdk.shutdown();
-  });
-
   it("should trace a generateText call", async () => {
-    const result = await generateText({
-      model: openai("gpt-3.5-turbo"),
+    const testParams = {
+      traceId: randomUUID(),
+      modelName: "gpt-3.5-turbo",
       maxTokens: 50,
       prompt: "Invent a new holiday and describe its traditions.",
-      maxToolRoundtrips: 3,
+      functionId: "test-vercel-generate-text",
+      userId: "some-user-id",
+      sessionId: "some-session-id",
+      metadata: {
+        something: "custom",
+        someOtherThing: "other-value",
+      },
+      tags: ["vercel", "openai"],
+    };
+
+    const { traceId, modelName, maxTokens, prompt, functionId, userId, sessionId, metadata, tags } = testParams;
+
+    const result = await generateText({
+      model: openai(modelName),
+      maxTokens,
+      prompt,
       experimental_telemetry: {
         isEnabled: true,
-        functionId: "my-awesome-function",
+        functionId,
         metadata: {
-          something: "custom",
-          someOtherThing: "other-value",
-          userId: "some-user-id",
-          sessionId: "some-session-id",
-          tags: ["vercel", "openai"],
+          langfuseTraceId: traceId,
+          userId,
+          sessionId,
+          tags,
+          ...metadata,
         },
       },
     });
 
-    console.log(result.text);
+    await sdk.shutdown();
+
+    // Fetch trace
+    const traceFetchResult = await fetchTraceById(traceId);
+    expect(traceFetchResult.status).toBe(200);
+
+    // Validate trace
+    const trace = traceFetchResult.data;
+
+    expect(trace.id).toBe(traceId);
+    expect(trace.name).toBe(functionId);
+    expect(JSON.parse(trace.input)).toEqual({ prompt });
+    expect(trace.output).toBe(result.text);
+    expect(trace.userId).toBe(userId);
+    expect(trace.sessionId).toBe(sessionId);
+    expect(trace.tags).toEqual(tags.sort());
+    expect(trace.metadata).toMatchObject(metadata);
+
+    // Validate generations
+    expect(trace.observations.length).toBeGreaterThan(0);
+    const generations = trace.observations.filter((o: any) => o.type === "GENERATION");
+    console.log(generations);
+
+    for (const generation of generations) {
+      expect(generation.input).toBeDefined();
+      expect(generation.output).toBeDefined();
+      expect(generation.model).toBe(modelName);
+      expect(generation.modelParameters).toMatchObject({ maxTokens: maxTokens.toString() });
+      expect(generation.calculatedInputCost).toBeGreaterThan(0);
+      expect(generation.calculatedOutputCost).toBeGreaterThan(0);
+      expect(generation.calculatedTotalCost).toBeGreaterThan(0);
+      expect(generation.promptTokens).toBeGreaterThan(0);
+      expect(generation.completionTokens).toBeGreaterThan(0);
+      expect(generation.totalTokens).toBeGreaterThan(0);
+    }
   }, 10_000);
+
   it("should work with toolCalls", async () => {
-    const traceId = randomUUID();
+    const testParams = {
+      traceId: randomUUID(),
+      functionId: "test-vercel-tool-call",
+      modelName: "gpt-3.5-turbo",
+      maxTokens: 512,
+      prompt: "What is the weather in San Francisco?",
+      userId: "some-user-id",
+      sessionId: "some-session-id",
+      metadata: {
+        something: "custom",
+        someOtherThing: "other-value",
+      },
+      tags: ["vercel", "openai"],
+    };
+
+    const { traceId, modelName, maxTokens, prompt, functionId, userId, sessionId, metadata, tags } = testParams;
 
     const result = await generateText({
-      model: openai("gpt-3.5-turbo"),
-      maxTokens: 512,
+      model: openai(modelName),
+      maxTokens,
+      prompt,
+      maxToolRoundtrips: 3,
       tools: {
         weather: weatherTool,
       },
-      prompt: "What is the weather in San Francisco?",
-      maxToolRoundtrips: 3,
       experimental_telemetry: {
         isEnabled: true,
-        functionId: "my-awesome-function",
+        functionId,
         metadata: {
-          something: "custom",
-          someOtherThing: "other-value",
-          userId: "some-user-id",
-          sessionId: "some-session-id",
-          tags: ["vercel", "openai"],
           langfuseTraceId: traceId,
+          userId,
+          sessionId,
+          tags,
+          ...metadata,
         },
       },
     });
 
-    console.log(result.text);
+    await sdk.shutdown();
+
+    // Fetch trace
+    const traceFetchResult = await fetchTraceById(traceId);
+    expect(traceFetchResult.status).toBe(200);
+
+    // Validate trace
+    const trace = traceFetchResult.data;
+
+    expect(trace.id).toBe(traceId);
+    expect(trace.name).toBe(functionId);
+    expect(JSON.parse(trace.input)).toEqual({ prompt });
+    expect(trace.output).toBe(result.text);
+    expect(trace.userId).toBe(userId);
+    expect(trace.sessionId).toBe(sessionId);
+    expect(trace.tags).toEqual(tags.sort());
+    expect(trace.metadata).toMatchObject(metadata);
+
+    // Validate generations
+    expect(trace.observations.length).toBeGreaterThan(0);
+    const generations = trace.observations.filter((o: any) => o.type === "GENERATION");
+    console.log(generations);
+
+    for (const generation of generations) {
+      expect(generation.input).toBeDefined();
+      expect(generation.output).toBeDefined();
+      expect(generation.model).toBe(modelName);
+      expect(generation.modelParameters).toMatchObject({ maxTokens: maxTokens.toString() });
+      expect(generation.calculatedInputCost).toBeGreaterThan(0);
+      expect(generation.calculatedOutputCost).toBeGreaterThan(0);
+      expect(generation.calculatedTotalCost).toBeGreaterThan(0);
+      expect(generation.promptTokens).toBeGreaterThan(0);
+      expect(generation.completionTokens).toBeGreaterThan(0);
+      expect(generation.totalTokens).toBeGreaterThan(0);
+    }
   }, 10_000);
-  //   it("should trace a streamText call", async () => {});
+
+  it("should trace a streamText call", async () => {
+    const testParams = {
+      traceId: randomUUID(),
+      functionId: "test-vercel-stream-text",
+      modelName: "gpt-3.5-turbo",
+      maxTokens: 512,
+      prompt: "Invent a new holiday and describe its traditions.",
+      userId: "some-user-id",
+      sessionId: "some-session-id",
+      metadata: {
+        something: "custom",
+        someOtherThing: "other-value",
+      },
+      tags: ["vercel", "openai"],
+    };
+
+    const { traceId, modelName, maxTokens, prompt, functionId, userId, sessionId, metadata, tags } = testParams;
+
+    const stream = await streamText({
+      model: openai(modelName),
+      maxTokens,
+      prompt,
+      experimental_telemetry: {
+        isEnabled: true,
+        functionId,
+        metadata: {
+          langfuseTraceId: traceId,
+          userId,
+          sessionId,
+          tags,
+          ...metadata,
+        },
+      },
+    });
+
+    let result = "";
+
+    for await (const chunk of stream.textStream) {
+      result += chunk;
+    }
+
+    console.log(result);
+
+    await sdk.shutdown();
+
+    // Fetch trace
+    const traceFetchResult = await fetchTraceById(traceId);
+    expect(traceFetchResult.status).toBe(200);
+
+    // Validate trace
+    const trace = traceFetchResult.data;
+
+    expect(trace.id).toBe(traceId);
+    expect(trace.name).toBe(functionId);
+    expect(JSON.parse(trace.input)).toEqual({ prompt });
+    expect(trace.output).toBe(result);
+    expect(trace.userId).toBe(userId);
+    expect(trace.sessionId).toBe(sessionId);
+    expect(trace.tags).toEqual(tags.sort());
+    expect(trace.metadata).toMatchObject(metadata);
+
+    // Validate generations
+    expect(trace.observations.length).toBeGreaterThan(0);
+    const generations = trace.observations.filter((o: any) => o.type === "GENERATION");
+    console.log(generations);
+
+    for (const generation of generations) {
+      expect(generation.input).toBeDefined();
+      expect(generation.output).toBeDefined();
+      expect(generation.model).toBe(modelName);
+      expect(generation.modelParameters).toMatchObject({ maxTokens: maxTokens.toString() });
+      expect(generation.calculatedInputCost).toBeGreaterThan(0);
+      expect(generation.calculatedOutputCost).toBeGreaterThan(0);
+      expect(generation.calculatedTotalCost).toBeGreaterThan(0);
+      expect(generation.promptTokens).toBeGreaterThan(0);
+      expect(generation.completionTokens).toBeGreaterThan(0);
+      expect(generation.totalTokens).toBeGreaterThan(0);
+    }
+  }, 10_000);
   //   it("should trace a generateObject call", async () => {});
   //   it("should trace a streamObject call", async () => {});
   //   it("should trace a embed call", async () => {});
