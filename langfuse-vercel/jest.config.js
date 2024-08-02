60961393ab9c77ed9230b4255001ad481c40a847
Hassieb Pakzad <68423100+hassiebp@users.noreply.github.com>
2024-08-02 15:17:23 +0200

    feat(vercel-ai): add integration for vercel-ai-sdk telemetry (#329)

---
+++ langfuse-vercel/jest.config.js
@@ -0,0 +1,16 @@
+// jest.config.js in each workspace package
+const baseConfig = require("../jest.config.js");
+
+module.exports = {
+  ...baseConfig,
+  transform: {
+    "^.+\\.ts?$": [
+      "ts-jest",
+      {
+        tsconfig: {
+          lib: ["ES2021", "ES2022.Error", "DOM"], // We need to include DOM for tests
+        },
+      },
+    ],
+  },
+};
