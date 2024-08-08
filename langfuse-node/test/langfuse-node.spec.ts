c8bb25c6b9fce67b517400f6883a14d4257afb12
Hassieb Pakzad <68423100+hassiebp@users.noreply.github.com>
2024-08-08 15:14:57 +0200

    fix test

--- langfuse-node/test/langfuse-node.spec.ts
+++ langfuse-node/test/langfuse-node.spec.ts
@@ -52,7 +52,7 @@ describe("Langfuse Node.js", () => {
         "http://example.com/api/public/ingestion",
         expect.objectContaining({
           method: "POST",
-          body: expect.stringMatching(/.*"id"\s*:\s*"test-id".*"name"\s*:\s*"test-trace-1".*/),
+          body: expect.stringMatching(/.*"id"\s*:\s*"test-id".*"name"\s*:\s*"trace-name".*/),
           headers: expect.objectContaining({
             "Content-Type": "application/json",
             Authorization: "Basic " + Buffer.from("pk:sk").toString("base64"),
