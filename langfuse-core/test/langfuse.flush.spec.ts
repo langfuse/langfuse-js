c8dbe524e3fd815a85c6ed849e6772a6f4fb1c6f
Adam Savitzky <adam.savitzky@ontailwind.com>
2025-01-27 08:51:57 -0800

    Respect batch size limits when flushing queue (#417)
    Co-authored-by: Hassieb Pakzad <68423100+hassiebp@users.noreply.github.com>

--- langfuse-core/test/langfuse.flush.spec.ts
+++ langfuse-core/test/langfuse.flush.spec.ts
@@ -244,7 +244,7 @@ describe("Langfuse Core", () => {
 
       expect(mocks.fetch).toHaveBeenCalledTimes(1);
     });
-
+    
     it("should not send events in admin mode", async () => {
       [langfuse, mocks] = createTestClient({
         publicKey: "pk-lf-111",
@@ -264,6 +264,37 @@ describe("Langfuse Core", () => {
       await jest.runAllTimersAsync();
 
       expect(mocks.fetch).not.toHaveBeenCalled();
+    });
+
+  describe("when queue is completely full", () => {
+    const MAX_MSG_SIZE = 1_000_000;
+    const BATCH_SIZE_LIMIT = 2_500_000;
+    // Message is right under the message size limit
+    const MSG_SIZE = MAX_MSG_SIZE - 1000;
+    const BIG_STRING = "a".repeat(MSG_SIZE);
+
+    it("should flush remaining items on subsequent flush", () => {
+      const n = Math.floor(BATCH_SIZE_LIMIT / MSG_SIZE) + 1;
+
+      [langfuse, mocks] = createTestClient({
+        publicKey: "pk-lf-111",
+        secretKey: "sk-lf-111",
+        flushAt: n,
+        flushInterval: 200,
+      });
+
+      // Adds enough messages to exceed batch size limit
+      for (let i = 0; i < n; i++) {
+        langfuse.trace({ name: `test-trace-${i}`, input: { content: BIG_STRING } });
+      }
+
+      // First call flushes the messages that fit under the batch size limit
+      expect(mocks.fetch).toHaveBeenCalledTimes(1);
+
+      jest.advanceTimersByTime(300);
+
+      // Second call flushes the remaining messages
+      expect(mocks.fetch).toHaveBeenCalledTimes(2);
     });
   });
 });
