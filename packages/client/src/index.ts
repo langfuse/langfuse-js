e03fa1748538d143abccff320a6aefb5e92ed12c
Hassieb Pakzad <68423100+hassiebp@users.noreply.github.com>
2025-09-03 18:12:13 +0200

    push

--- packages/client/src/index.ts
+++ packages/client/src/index.ts
@@ -3,3 +3,5 @@ export * from "./prompt/index.js";
 export * from "./score/index.js";
 export * from "./dataset/index.js";
 export * from "./media/index.js";
+export * from "./experiment/Experiment.js";
+export * from "./experiment/ExperimentManager.js";
